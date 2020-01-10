584abe8548218d21bdbee7ca218296cb786a1165
Kuen Hwi <kueniyam@naver.com>
2020-01-10 09:17:22 +0900

    공부 3일차

--- sample/polls/models.py
+++ sample/polls/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Question(models.Model):
+    objects = models.Manager()
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
@@ -10,6 +11,7 @@ class Question(models.Model):
         return self.question_text
 
 class Choice(models.Model):
+    objects = models.Manager()
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
